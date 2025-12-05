146f18eb5a1b27b9577892dafc47dc3714a72c95
George Mukundi <mukundichabari@gmail.com>
2025-12-05 14:21:05 +0300

    m

--- mtolori_api/invoice.py
+++ mtolori_api/invoice.py
@@ -98,9 +98,12 @@ def create(**args):
                     sales_invoice_doc.set_missing_values()
                     frappe.flags.ignore_account_permission = True
                     sales_invoice_doc.save(ignore_permissions = True)
+                    frappe.db.commit() 
                                     
                     sign_invoice(sales_invoice_doc)
                     
+                    sales_invoice_doc.flags.ignore_permissions = True
+                    frappe.flags.ignore_account_permission = True
                     sales_invoice_doc.submit()
                     frappe.db.commit() 
                     
